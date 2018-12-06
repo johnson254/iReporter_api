f9905d7a8aa97c3601de0cc75a576804bba02872
johnson254 <wambirujohnson@gmail.com>
2018-12-06 09:44:42 +0300

    [Finishes #162469543]Updates travis file

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app.api.api import app
+import os
+from app import create_app
+
+config_name = os.getenv("development")
+app = create_app(config_name)
 
 if __name__ == "__main__":
-     app.run(debug=True)
+     app.run()
+
+
